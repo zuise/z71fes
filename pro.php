e807d395ed4988a74c35d8d24f3254c346aecc2f
zuise <43239500+zuise@users.noreply.github.com>
2018-09-13 23:50:01 +0900

    Update pro.php

--- pro.php
+++ pro.php
@@ -1,54 +1,60 @@
-$midFile = dirname(__FILE__)."/mids";
-$json = json_decode(file_get_contents("php://input"), true);
-$mids = explode(PHP_EOL, trim(file_get_contents($midFile)));
-$newMids = array();
-if (!isset($json["result"])) {
-    exit(0);
-}
-foreach ($json["result"] as $result) {
-    $newMids[] = $result["content"]["from"];
-}
-$messages = array();
-foreach ($json["result"] as $result) {
-    if (!isset($result["content"]["text"])) {
-        continue;
-    }
-    if (1 > strlen($result["content"]["text"])) {
-        continue;
-    }
-    $messages[] = array(
-        "contentType" => 1,
-        "text" => $result["content"]["text"],
-    );
-}
-if (0 == count($messages)) {
-    exit(0);
-}
-$mids = array_merge($newMids, $mids);
-$mids = array_unique($mids);
-file_put_contents($midFile, implode(PHP_EOL, $mids));
+<?php
 
-$body = json_encode(
-array(
-    "to" => array_values($mids),
-    "toChannel" => 1383378250,
-    "eventType" => "140177271400161403",
-    "content" => array(
-        "messageNotified" => 0,
-        "messages" => $messages,
-    ),
-)
-);
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://trialbot-api.line.me/v1/events");
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-"Content-Type: application/json; charset=UTF-8",
-"X-Line-ChannelID: 1607264057",
-"X-Line-ChannelSecret: 51f6f641a0fa156eea2ed7faa36c13f3
-",
-"X-Line-Trusted-User-With-ACL: xNqP/18XS1eEHyWEOk9kmz2PfjMf/1v9eQWdB/yVFBitRW5Wic7hf3u1k/kZE5vJGqgnwzYCBVh9fC6NQhzkccE/4r5FXMWv9CCNOTWZ22B0oa2u/7hzvIsrn/2q7m5xf+LoJ8VPP16fdAgbnpKSjgdB04t89/1O/w1cDnyilFU=
-",
-));
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-$result = curl_exec($ch);
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+// 不正であれば例外の内容を出力
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
+
+$midFile = __DIR__ . "/files/mids";
+
+// midsの中身を読み込み
+$mids = explode(PHP_EOL, trim(file_get_contents($midFile)));
+
+
+// 配列に格納された各イベントをループで処理
+foreach ($events as $event) {
+
+  //  テキストメッセージでなければ処理をスキップ
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+    error_log('Non message event has come');
+    continue;
+  }
+
+  // メッセージを送ってきたユーザーを取得
+  $newMids = array();
+  $newMids[] = $event->getUserId();
+
+　// 新規のユーザーの場合は追加
+  $mids = array_merge($newMids, $mids);
+  $mids = array_unique($mids);
+
+  file_put_contents($midFile, implode(",", $mids));
+
+  // メッセージを全登録ユーザーID宛にプッシュ
+  foreach ($mids as $mid) {
+    $response = $bot->pushMessage($mid, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText()));
+    if (!$response->isSucceeded()) {
+      error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+    }
+  }
+
+}
